71fbbe6a1ccff05990075e9630d5dbd76c704732
D1K7 <dhruvkashyap1701@gmail.com>
2025-10-23 15:55:26 +0530

    Ordinal Encoding 2

--- ordinal_encoding.py
+++ ordinal_encoding.py
@@ -7,13 +7,16 @@ def ordinalencoder(df,column_name):
     distinct_values=set(df[column_name])
     c=0
     d={}
+    
 
     for i in distinct_values:
         d[i]=c
         c+=1
-    print(d)
+    
 
     df[column_name]=df[column_name].map(d)
+    return df
+    
 
-ordinalencoder(df,"SaleCondition")
+df=ordinalencoder(df,"SaleCondition")
 print(df["SaleCondition"])
